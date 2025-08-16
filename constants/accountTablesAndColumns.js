94dfb94bfb8e6e86823cfb136dba33bf371a5001
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-08-16 10:53:13 +0530

    creating account tables and columns list

---
+++ constants/accountTablesAndColumns.js
@@ -0,0 +1,154 @@
+import { columns } from "mssql";
+
+export const ACCOUNT_TABLES_COLUMNS = [
+    {
+        table: 'Master1',
+        columns: [
+            'Code',
+            'MasterType',
+            'Name',
+            'Alias',
+            'PrintName',
+            'ParentGrp',   // Account Group
+            'Stamp',
+            'CreatedBy',
+            'CreationTime',
+            'ModifiedBy',
+            'ModificationTime',
+            'AuthorizedBy',
+            'AuthorizationTime',
+            'ApprovalStatus',
+            'SyncStatus',
+            'MasterSeriesGrp',
+            'Source',
+            'BlockedMaster',
+            'DeactiveMaster',
+            'TPF1',
+            'TPF2',
+            'RejectionStatus',
+            'AppSync'
+        ]
+    },
+    {
+        table: 'MasterAddressInfo',
+        columns: [
+            'Code',
+            'Address1',
+            'Address2',
+            'Address3',
+            'Address4',
+            'TelNo',
+            'Fax',
+            'Email',
+            'Mobile',
+            'LST',
+            'CST',    // SVAT
+            'ST37',
+            'TINNo',   // VAT
+            'LBTNo',
+            'ITPAN',
+            'ITWard',
+            'Contact',
+            'CountryCodeLong', // country code -> from Master1 table master code
+            'StateCodeLong',
+            'CityCodeLong',
+            'RegionCodeLong',
+            'AreaCodeLong',
+            'ContDeptCodeLong',
+            'PINCode',
+            'Transport',
+            'Station',
+            'AccNo',
+            'Date1',
+            'Date2',
+            'Date3',
+            'Date4',
+            'Date5',
+            'Date6',
+            'Date7',
+            'Date8',
+            'C4',  // Bank Name
+            'C5', // IFSC Code
+            'Distance',
+            'TransportMode',
+            'WhatsAppNo'
+        ]
+    },
+    {
+        table: 'Help1',
+        columns: [
+            'Code',
+            'ReqType',
+            'NameAlias',
+            'NameOrAlias',  // if 1 NameAlias is acc name, if 2 it is alias
+            'MasterType',
+            'AdditionalInfo',
+            'ParentGrp',   // Account Group
+            'MasterSeries',
+            'Status',
+            'DefaultMCCode'
+        ]
+    },
+    {
+        table: 'MasterFootPrint',
+        columns: [
+            'Name',
+            'MasterCode',
+            'MasterType',
+            'Stamp',
+            'PartialFP',
+            'RemainFP',
+        ]
+    },
+    {
+        table: 'Help1AddnInfo',
+        columns: [  // there are 2 rows (or more) for each account. 1 has NameAlias and 1 has NameOrAlias 2 
+            'Code',
+            'NameAlias',
+            'NameOrAlias',  // if 1 NameAlias is acc name, if 2 it is alias
+            'AddnInfoBT1',  // -> Address : Address1, Address2
+            'AddnInfoBT2',  // ->  Address3, Address4
+            'AddnInfoBT3',  // -> Contact : Contact Person / Tel No
+            'AddnInfoBT4',  // -> TIN/CST : VAT/SVAT
+        ]
+    },
+    {
+        table: 'Folio1',
+        columns: [
+            'MasterCode',
+            'MasterType',
+            'D1', // Opening Balance
+            'D2',
+            'D3',
+            'D4', // Prev Year Balance
+            'D5',
+            'D6',
+            'D7',
+            'D8',
+            'D9',
+            //  there are to D150
+            'B1',
+            'B2',
+            'B3',
+            'B4',
+            'B5',
+            'B6',
+            'B7',
+            'B8',
+            'B9',
+            'B10',
+            'B11',
+            'B12'
+        ]
+    },
+    {
+        table: 'Images',
+        columns: [
+            'Code',
+            'Image1', // { type: "Image", "Origin": "dnwanljdwjj12381" }
+            'FormatType1', // Image format type (e.g., 'png', 'jpg')
+            'Image2', // { type: "Image", "Origin": "dnwanljdwjj12382" }
+            'FormatType2', // Image format type (e.g., 'png', 'jpg')
+        ]
+    }
+]
--- services/account.js
+++ services/account.js
@@ -1,15 +1,25 @@
 import { companyOperationPool } from "../config/companyOperationsDb.js";
+import { MASTER_TYPES } from "../constants/masterTypes.js";
 
 const pool = await companyOperationPool()
 
+const accountTablesColumns = [
+    { table: 'Master1', columns: ['Code', 'Name'] },
+    { table: 'Master2', columns: ['Code', 'Name'] },
+    { table: 'Master3', columns: ['Code', 'Name'] },
+    { table: 'Master4', columns: ['Code', 'Name'] },
+    { table: 'Master5', columns: ['Code', 'Name'] },
+    { table: 'Master6', columns: ['Code', 'Name'] }
+];
+
 export const getAllAccounts = async () => {
-    const accounts = await pool.request().query('SELECT Code, Name FROM Master1 WHERE MasterType = 2');
+    const accounts = await pool.request().query(`SELECT Code, Name FROM Master1 WHERE MasterType = ${MASTER_TYPES.ACCOUNTS}`);
     return accounts
 }
 
 export const getAccountByCode = async (code) => {
     const account = await pool.request()
         .input('Code', code)
-        .query('SELECT Code, Name FROM Master1 WHERE MasterType = 2 AND Code = @Code');
+        .query(`SELECT Code, Name FROM Master1 WHERE MasterType = ${MASTER_TYPES.ACCOUNTS} AND Code = @Code`);
     return account
 }
